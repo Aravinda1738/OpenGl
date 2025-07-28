554b0281c41cbe11c5d1023312aa1dd59cdb8a07
Aravinda <Aravinda@DESKTOP-0357U8Q>
2025-07-28 15:02:11 +0530

    Shader creation (in progress)

--- OpenGl/SRC/Application.cpp
+++ OpenGl/SRC/Application.cpp
@@ -1,6 +1,25 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+
+
+
+static unsigned int CompileShaders(unsigned int type,const std::string& source ) {
+
+    unsigned int id = glCreateShader(GL_VERTEX_SHADER);
+    const char* src = source.c_str();
+    glShaderSource(id,1,&src,nullptr);
+    glCompileShader(id);
+
+}
+
+static int CreateShader(const std::string& vertexShader,const std::string& fragmentShader) {
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShaders(GL_VERTEX_SHADER, vertexShader);
+
+
+}
+
 int main(void)
 {
     GLFWwindow* window;
@@ -24,6 +43,22 @@ int main(void)
         std::cout << glGetString(GL_VERSION) << std::endl;
     }
     
+    float positions[6] = {
+    -0.5f,-0.5f,
+    0.5f, 1.0f,
+    1.0f, 0.5f
+    };
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER,buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+   
+    
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0,2,GL_FLOAT,GL_FALSE,sizeof(float)*2,0);
+
+     
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -31,12 +66,7 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
        
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.5f, 1.0f);
-        glVertex2f(1.0f, 0.5f);
-      //testing commit
-        glEnd();
+        glDrawArrays(GL_TRIANGLES,0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
