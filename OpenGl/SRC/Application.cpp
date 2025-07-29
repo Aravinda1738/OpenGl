93718179e584d31929e3b423b46506a3af8e3745
Aravinda <Aravinda@DESKTOP-0357U8Q>
2025-07-29 12:42:10 +0530

    #put shader in a seperate file and read from it. #done for today need to recap.

--- OpenGl/SRC/Application.cpp
+++ OpenGl/SRC/Application.cpp
@@ -3,21 +3,51 @@
 #include <iostream>
 //#include <random>
 #include <string>
+#include <sstream>
 #include <fstream>
-static void ParseShader(const std::string& filepath) {
+
+
+
+
+struct ShaderProgramSourse {
+    std::string VertexSourse;
+    std::string FragmentSource;
+};
+
+
+
+static ShaderProgramSourse ParseShader(const std::string& filepath) {
 
     std::ifstream stream(filepath);
+
+    enum class ShaderType {
+        NONE = -1, VERTEX = 0, FRAGMENT = 1
+    };
+
+
+
     std::string line;
+    std::stringstream ss[2];
+    ShaderType type = ShaderType::NONE;
     while (getline(stream,line))
     {
         if (line.find("shader") != std::string::npos)
         {
-            if (line.find("vertix") != std::string::npos {
-                //
+            if (line.find("vertex") != std::string::npos ){
+               type= ShaderType::VERTEX;
             }
+            else if (line.find("fragment") != std::string::npos)
+            {
+                type = ShaderType::FRAGMENT;
+            }
+           
+        }
+        else
+        {
+            ss[(int)type] << line << '\n';
         }
     }
-
+    return{ ss[0].str(),ss[1].str() };
 }
 
 
@@ -100,11 +130,17 @@ int main(void)
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0,2,GL_FLOAT,GL_FALSE,sizeof(float)*2,0);
 
-  /*  std::random_device rd;
-    std::mt19937 eng(rd());
-    std::uniform_real_distribution<> distr(0.0, 1.0);     random num between 0.0,1.0
-    double randomNumber = distr(eng);
-    std::string num = std::to_string(randomNumber);*/
+ 
+    ShaderProgramSourse sourse = ParseShader("res/shaders/Basic.shader"); 
+    std::cout << "Vertex" << std::endl;
+    std::cout << sourse.VertexSourse << std::endl;
+    std::cout << "Fragment" << std::endl;
+    std::cout << sourse.FragmentSource << std::endl;
+
+
+    unsigned int shader = CreateShader(sourse.VertexSourse, sourse.FragmentSource);
+    glUseProgram(shader);
+
 
 
     
@@ -124,7 +160,7 @@ int main(void)
         /* Poll for and process events */
         glfwPollEvents();
     }
-
+   glDeleteProgram(shader);
     glfwTerminate();
     return 0;
 }
